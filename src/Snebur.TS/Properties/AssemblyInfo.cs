05ed4eb9cef4902cf89905b303420f3c518e2feb
Rubens Cordeiro <zyoncore@hotmail.com>
2022-12-06 10:39:41 -0300

    adicionar arquivos

---
+++ src/Snebur.TS/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Snebur.TS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Snebur")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fac4c394-3a64-463a-b333-4e25350de756")]
+[assembly: AssemblyVersion("1.0.2110.100")]
+[assembly: AssemblyFileVersion("1.0.2110.100")]
