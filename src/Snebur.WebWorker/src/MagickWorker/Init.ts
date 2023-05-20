005df88e5fa23e05698df21c62897c19ad42e6f5
Rubens Cordeiro <zyoncore@hotmail.com>
2023-05-20 17:00:46 -0300

    ajustes do magick worker

--- src/Snebur.WebWorker/src/MagickWorker/Init.ts
+++ src/Snebur.WebWorker/src/MagickWorker/Init.ts
@@ -1,7 +1,7 @@
 ﻿let isMagickCarregado = false;
 let isInicilizando = false;
 
-async function inicializarMagickAsync(mensagem: IOpcoesMagick)
+async function inicializarMagickAsync(mensagem: IOpcoesMagick): Promise<boolean>
 {
     if (isInicilizando)
     {
@@ -19,22 +19,31 @@ async function inicializarMagickAsync(mensagem: IOpcoesMagick)
             importScripts(mensagem.UrlMagick);
 
             /*const bytes = new Uint8Array(mensagem.BufferWasm);*/
-            const blobWasm = new Blob([mensagem.BufferWasm], { type: "application/wasm" });
-            const url = self.URL.createObjectURL(blobWasm);
+            /*const blobWasm = new Blob([mensagem.BufferWasm], { type: "application/wasm" });*/
+            /*const url = self.URL.createObjectURL(blobWasm);*/
+
+            const url = self.URL.createObjectURL(mensagem.BlobWasm);
 
             await MagickWasm.initializeImageMagick(url);
             /*await MagickWasm.initializeImageMagick("/magick/magick.wasm");*/
             self.URL.revokeObjectURL(url);
             if (MagickWasm.Magick.imageMagickVersion != null)
             {
-                console.log("Magick worker Carregado v:" + MagickWasm.Magick.imageMagickVersion);
                 isMagickCarregado = true;
+                console.log("Magick worker Carregado v:" + MagickWasm.Magick.imageMagickVersion);
+                return true;
             }
+        }
+        catch (erro)
+        {
+            console.error("Erro Magick worker Carregado " + erro);
+            self.postMessage(erro);
         }
         finally
         {
             isInicilizando = false;
         }
+        return false;
     }
 
 }
--- src/Snebur.WebWorker/src/MagickWorker/MagickWorker.ts
+++ src/Snebur.WebWorker/src/MagickWorker/MagickWorker.ts
@@ -4,10 +4,13 @@ self.addEventListener("message", async function (e: MessageEvent)
     const opcoes = e.data as IOpcoesMagick;
     try
     {
-        await inicializarMagickAsync(opcoes);
-        const processador = new MagickProcessador(opcoes);
-        const resultado = await processador.ProcessarAsync();
-        self.postMessage(resultado);
+        const isSucesso = await inicializarMagickAsync(opcoes);
+        if (isSucesso)
+        {
+            const processador = new MagickProcessador(opcoes);
+            const resultado = await processador.ProcessarAsync();
+            self.postMessage(resultado);
+        }
     }
     catch (erro)
     {
