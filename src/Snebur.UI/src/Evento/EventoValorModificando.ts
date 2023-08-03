80e561f860ae1dd3fee685f8f86d55a7e55b8c4c
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-03 19:21:38 -0300

    renomeado EventoValorModificando

--- src/Snebur.UI/src/Evento/EventoValorModificando.ts
+++ src/Snebur.UI/src/Evento/EventoValorModificando.ts
@@ -1,16 +1,16 @@
 ﻿namespace Snebur.UI
 {
-    export class EventoValorMotificando extends BaseEventoValorAlterado
+    export class EventoValorModificando extends BaseEventoValorAlterado
     {
         public constructor(controlePai: BaseControle, elemento: HTMLElement)
         {
             super(controlePai, elemento, AtributosHtml.ValorModificando, ui.EnumEventoDom.Input);
 
-            if (!((controlePai as IControleEventoValorModificando<any>).EventoValorMotificando instanceof Evento))
+            if (!((controlePai as IControleEventoValorModificando<any>).EventoValorModificando instanceof Evento))
             {
                 throw new ErroNaoSuportado("controle  pai não implementa  EventoValorMotificando", this);
             }
-            (controlePai as IControleEventoValorModificando<any>).EventoValorMotificando.AddHandler(this.Controle_ValorAlterado, this);
+            (controlePai as IControleEventoValorModificando<any>).EventoValorModificando.AddHandler(this.Controle_ValorAlterado, this);
         }
     }
 }
