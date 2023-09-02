2b4c1be2d962dd34a35d28d1c9fa8bae01ae0361
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-01 21:34:11 -0300

    adicionado novas cores

--- src/Snebur.TS/src/Comum/Cores.ts
+++ src/Snebur.TS/src/Comum/Cores.ts
@@ -37,5 +37,60 @@ namespace Snebur
         {
             return new d.Cor(150, 150, 150);
         }
+
+        public static get Amarelo(): d.Cor
+        {
+            return new d.Cor(255, 255, 0);
+        }
+
+        public static get Rosa(): d.Cor
+        {
+            return new d.Cor(255, 0, 255);
+        }
+
+        public static get Roxo(): d.Cor
+        {
+            return new d.Cor(128, 0, 128);
+        }
+
+        public static get Laranja(): d.Cor
+        {
+            return new d.Cor(255, 165, 0);
+        }
+
+        public static get Marrom(): d.Cor
+        {
+            return new d.Cor(165, 42, 42);
+        }
+
+        public static get VerdeEscuro(): d.Cor
+        {
+            return new d.Cor(0, 100, 0);
+        }
+
+        public static get AzulEscuro(): d.Cor
+        {
+            return new d.Cor(0, 0, 139);
+        }
+
+        public static get CinzaEscuro(): d.Cor
+        {
+            return new d.Cor(105, 105, 105);
+        }
+
+        public static get AmareloEscuro(): d.Cor
+        {
+            return new d.Cor(255, 215, 0);
+        }
+
+        public static get RosaEscuro(): d.Cor
+        {
+            return new d.Cor(199, 21, 133);
+        }
+
+        public static get RoxoEscuro(): d.Cor
+        {
+            return new d.Cor(75, 0, 130);
+        }
     }
 }
