029460ec08394d0811d04fe99b15cd56bcabe391
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-01 21:31:51 -0300

    Adicionado ValidacaoMd5

---
+++ src/Snebur.TS/src/Dominio/Atributos/Validacao/ValidacaoMdr5.Partial.ts
@@ -0,0 +1,28 @@
+﻿
+namespace Snebur.Dominio.Atributos
+{
+    export interface ValidacaoMdr5Attribute
+    {
+        RetornarMensagemValidacao(paiPropriedade: any, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): string;
+
+        IsValido(paiPropriedade: Snebur.Dominio.BaseDominio, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): boolean;
+
+    }
+
+    ValidacaoMdr5Attribute.prototype.RetornarMensagemValidacao = function (paiPropriedade: any, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): string
+    {
+        const rotuloPropriedade = u.GlobalizacaoUil.RetornarRotuloPropriedade(propriedade).toLowerCase();
+        const mensagemValidacao = u.GlobalizacaoUil.RetornarMensagemValidacao(this, ValidacaoMdr5Attribute.IDENTIFICADOR_MENSAGEM_VALIDACAO, rotuloPropriedade);
+        return mensagemValidacao;
+    };
+
+    ValidacaoMdr5Attribute.prototype.IsValido = function (paiPropriedade: Snebur.Dominio.BaseDominio, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): boolean
+    {
+        if (!String.IsNullOrWhiteSpace((valorPropriedade)))
+        {
+            return u.ValidacaoUtil.IsMd5(valorPropriedade);
+        }
+        return true;
+    };
+}
+
