6a0956c1408d1f05c644884839f83a7bfc844788
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-01 21:31:43 -0300

    adicionado ValidacaoGuid

---
+++ src/Snebur.TS/src/Dominio/Atributos/Validacao/ValidacaoGuid.Partial.ts
@@ -0,0 +1,28 @@
+﻿
+namespace Snebur.Dominio.Atributos
+{
+    export interface ValidacaoGuidAttribute
+    {
+        RetornarMensagemValidacao(paiPropriedade: any, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): string;
+
+        IsValido(paiPropriedade: Snebur.Dominio.BaseDominio, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): boolean;
+
+    }
+
+    ValidacaoGuidAttribute.prototype.RetornarMensagemValidacao = function (paiPropriedade: any, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): string
+    {
+        const rotuloPropriedade = u.GlobalizacaoUil.RetornarRotuloPropriedade(propriedade).toLowerCase();
+        const mensagemValidacao = u.GlobalizacaoUil.RetornarMensagemValidacao(this, ValidacaoGuidAttribute.IDENTIFICADOR_MENSAGEM_VALIDACAO, rotuloPropriedade);
+        return mensagemValidacao;
+    };
+
+    ValidacaoGuidAttribute.prototype.IsValido = function (paiPropriedade: Snebur.Dominio.BaseDominio, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): boolean
+    {
+        if (!String.IsNullOrWhiteSpace((valorPropriedade)))
+        {
+            return u.ValidacaoUtil.IsGuid(valorPropriedade);
+        }
+        return true;
+    };
+}
+
