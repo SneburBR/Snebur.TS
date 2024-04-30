dbd27922fc5bd27339180cd953246186a5627e5e
Rubens Cordeiro <zyoncore@hotmail.com>
2024-04-30 14:53:31 -0300

    adicionado Queue

---
+++ src/Snebur.TS/src/Core/Lista/Queue.ts
@@ -0,0 +1,51 @@
+﻿namespace Snebur
+{
+	export class Queue<T>
+	{
+		private _items = new LinkedList<T>();
+		public get Count(): number
+		{
+			return this._items.Count;
+		}
+
+		public Enqueue(item: T): void
+		{
+            this._items.Add(item);
+		}
+
+		public Contains(item: T): boolean
+		{
+			return this._items.Contains(item);
+		}
+
+		public Remove(item: T):boolean
+		{
+			return this._items.Remove(item);
+		}
+
+		public Dequeue(): T | undefined
+        {
+            if (this.Count === 0)
+            {
+                return undefined;
+			}
+
+            const item = this._items.Head!.Value;
+            this._items.Remove(item);
+            return item;
+		}
+
+		public Peek(): T | undefined
+		{
+			return this._items.Head?.Value;
+		}
+
+		public Clear(): void
+		{
+			return this._items.Clear();
+		}
+	}
+
+	
+	
+}
