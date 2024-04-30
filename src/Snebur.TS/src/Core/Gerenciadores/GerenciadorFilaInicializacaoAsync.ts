c8a72992b0aab81cd0bc104c9357b9f90e3763f6
Rubens Cordeiro <zyoncore@hotmail.com>
2024-04-30 14:53:50 -0300

    adicionado GerenciadorFilaInicializacaoAsync

---
+++ src/Snebur.TS/src/Core/Gerenciadores/GerenciadorFilaInicializacaoAsync.ts
@@ -0,0 +1,46 @@
+﻿
+namespace Snebur
+{
+    export class GerenciadorFilaInicializacaoAsync
+    {
+        private readonly Fila = new Queue<IInicializacaoAsync>();
+        private ItemAtual: IInicializacaoAsync | null = null;
+
+        public Adicionar(item: IInicializacaoAsync): void
+        {
+            this.Fila.Enqueue(item);
+        }
+
+        public Remover(item: IInicializacaoAsync): void
+        {
+            this.Fila.Remove(item);
+        }
+
+        public async ExecutarAsync(): Promise<void>
+        {
+            if (this.ItemAtual != null || this.Fila.Count === 0)
+            {
+                return;
+            }
+
+            this.ItemAtual = this.Fila.Dequeue();
+            if (this.ItemAtual != null)
+            {
+                try
+                {
+                    await this.ItemAtual.InicializarAsync();
+                }
+                finally
+                {
+                    this.ItemAtual = null;
+                }
+            }
+            await this.ExecutarAsync();
+        }
+
+        public Limpar(): void
+        {
+            this.Fila.Clear();
+        }
+    }
+}
