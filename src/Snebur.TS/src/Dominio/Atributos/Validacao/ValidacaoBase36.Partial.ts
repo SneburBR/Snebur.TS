e71bb5bd3ecc62663eba40a7a743f208ec603c01
Rubens Cordeiro <zyoncore@hotmail.com>
2024-05-23 18:58:15 -0300

    adicionado ValidacaoBase36

---
+++ src/Snebur.TS/src/Dominio/Atributos/Validacao/ValidacaoBase36.Partial.ts
@@ -0,0 +1,28 @@
+﻿
+namespace Snebur.Dominio.Atributos
+{
+    export interface ValidacaoBase36Attribute
+    {
+        RetornarMensagemValidacao(paiPropriedade: any, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): string;
+
+        IsValido(paiPropriedade: Snebur.Dominio.BaseDominio, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): boolean;
+
+    }
+
+    ValidacaoBase36Attribute.prototype.RetornarMensagemValidacao = function (paiPropriedade: any, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): string
+    {
+        const rotuloPropriedade = u.GlobalizacaoUil.RetornarRotuloPropriedade(propriedade).toLowerCase();
+        const mensagemValidacao = u.GlobalizacaoUil.RetornarMensagemValidacao(this, ValidacaoBase36Attribute.IDENTIFICADOR_MENSAGEM_VALIDACAO, rotuloPropriedade);
+        return mensagemValidacao;
+    };
+
+    ValidacaoBase36Attribute.prototype.IsValido = function (paiPropriedade: Snebur.Dominio.BaseDominio, propriedade: Snebur.Reflexao.Propriedade, valorPropriedade: any): boolean
+    {
+        if (!String.IsNullOrWhiteSpace((valorPropriedade)))
+        {
+            return u.Base36Util.IsBase36(valorPropriedade, this.IsPermitirEspaco, this.IsIgnorarCase);
+        }
+        return true;
+    };
+}
+
