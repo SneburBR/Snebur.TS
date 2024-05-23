6eff5ecca6143bc6399280cbc28b9cd6e705938e
Rubens Cordeiro <zyoncore@hotmail.com>
2024-05-23 18:58:24 -0300

    adicionado Base36Util

---
+++ src/Snebur.TS/src/Utilidade/Base36Util.ts
@@ -0,0 +1,62 @@
+﻿namespace Snebur.Utilidade
+{
+	export class Base36Util
+    {
+        public static IsBase36(valor: string, isPermitirEspaco:boolean, isIgnorarCase:boolean): boolean
+        {
+            if (String.IsNullOrWhiteSpace(valor))
+            {
+                return false;
+            }
+            if (isIgnorarCase)
+            {
+                valor = valor.toUpperCase();
+            }
+
+            if (isPermitirEspaco)
+            {
+                //[A-Z0-9 ]+$
+                const padrao = /^[A-Z0-9 ]+$/;  
+                return padrao.test(valor);
+            }
+
+            //[A-Z0-9]+$
+            const padrao = /^[A-Z0-9]+$/;
+            return padrao.test(valor);
+        }
+
+        public static Converter(valor: number, minimoLetras:number=0): string
+        {
+            const base = 36;
+            const letras = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            let resultado = "";
+            while (valor > 0)
+            {
+                const resto = valor % base;
+                resultado = letras[resto] + resultado;
+                valor = Math.floor(valor / base);
+            }
+
+            if (resultado.length < minimoLetras)
+            {
+                return "0".repeat(minimoLetras - resultado.length) + resultado;
+            }
+            return resultado;
+        }
+
+        public static RetornarId(base36: string): number
+        {
+            const base = 36;
+            const letras = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            let resultado = 0;
+            for (let i = 0; i < base36.length; i++)
+            {
+                const letra = base36[i];
+                const valor = letras.indexOf(letra);
+                resultado = (resultado * base) + valor;
+            }
+            return resultado;
+        }
+               
+	}
+}
