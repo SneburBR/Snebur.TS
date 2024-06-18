d9c2d8f9c68bc5141f6ffb2aff5c93a7b41a7fca
Rubens Cordeiro <zyoncore@hotmail.com>
2024-06-18 21:55:23 +0200

    adicionado Set.ts

---
+++ src/Snebur.TS/src/Core/Nativo/Set.ts
@@ -0,0 +1,25 @@
+﻿namespace Snebur
+{
+    Set.prototype.AddRangeNew = (function (this: Set<any>, itens: any[])
+    {
+        /*eslint-disable*/
+        this.Clear();
+        this.AddRange(itens);
+    });
+
+    Set.prototype.AddRange = (function (this: Set<any>, itens: any[])
+    {
+        if (Array.isArray(itens) && itens.length > 0)
+        {
+            for (const item of itens)
+            {
+                this.add(item);
+            }
+        }
+    });
+
+    Set.prototype.Clear = (function (this: Set<any>)
+    {
+        this.clear();
+    });
+}
