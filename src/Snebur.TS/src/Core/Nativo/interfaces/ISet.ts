826b042202fb10e930ba0d490f7b7a9fcae36d03
Rubens Cordeiro <zyoncore@hotmail.com>
2024-06-18 21:55:13 +0200

    adicionado ISet

---
+++ src/Snebur.TS/src/Core/Nativo/interfaces/ISet.ts
@@ -0,0 +1,15 @@
+﻿interface Set<T> extends Snebur.Nativo.ISet<T>
+{
+
+}
+
+
+namespace Snebur.Nativo
+{
+    export interface ISet<T>
+    {
+        AddRange(itens: Array<any>): void;
+        AddRangeNew(itens: Array<any>): void;
+        Clear(): void;
+    }
+}
