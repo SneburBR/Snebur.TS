e817bd94ece792dcdcc3595a2a75dc8ebeaafd4f
Rubens Cordeiro <zyoncore@hotmail.com>
2024-08-12 19:59:51 +0200

    Add Snebur namespace with ItemImagemEventArgs class
    Introduced a new namespace `Snebur` and added the `ItemImagemEventArgs` class within it. This class extends `EventArgs` and includes a public readonly property `Imagem` of type `d.IImagem`. The constructor initializes the `Imagem` property and calls the base class constructor.

---
+++ src/Snebur.TS/src/Imagem/ItemImagemEventArgs.ts
@@ -0,0 +1,12 @@
+﻿namespace Snebur
+{
+    export class ItemImagemEventArgs extends EventArgs
+    {
+        public readonly Imagem: d.IImagem;
+        public constructor(imagem: d.IImagem)
+        {
+            super();
+            this.Imagem = imagem;
+        }
+    }
+}
