5b2ff2e9a7664fa186ed26819c74e4429a36e19e
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-06-23 11:39:06 -0300

    Refactor SnBlob: nullability, caching, and cleanup
    Refactored SnBlob to explicitly handle nullability for private fields and added caching for computed properties (Name, Size, Type, etc.) to improve efficiency. Enhanced resource cleanup in RevokeUrlBlob and Dispose, and improved debug information in ValidarSeDispensado. Updated method structure for consistency and maintainability.

--- src/Snebur.TS/src/Arquivo/SnBlob.ts
+++ src/Snebur.TS/src/Arquivo/SnBlob.ts
@@ -1,117 +1,154 @@
-﻿namespace Snebur {
-    export class SnBlob extends Snebur.SneburObject {
-        private _nomeArquivo: string;
-        private _informacaoImagem: IInformacaoImagem;
+﻿namespace Snebur
+{
+    export class SnBlob extends Snebur.SneburObject
+    {
+        private _nomeArquivo: string | null = null;
+        private _informacaoImagem: IInformacaoImagem | null = null;
+        private _name: string | null = null;
         private _isDispensado: boolean = false;
-        private _urlBlob: string = null;
-        private _checksum: string | Error = null;
+        private _urlBlob: string | null = null;
+        private _checksum: string | Error | null = null;
         private _blob: Blob;
+        private _buffer: ArrayBuffer | null = null;
+        private _size: number | null = null;
+        private _type: string | null = null;
 
-        public get IsHeic(): boolean {
+        public get IsHeic(): boolean
+        {
             return this._informacaoImagem?.IsHeic;
         }
 
-        public get IsFormatoImagemSuportado(): boolean {
+        public get IsFormatoImagemSuportado(): boolean
+        {
             return true;
         }
 
-        public get Blob(): Blob {
+        public get Blob(): Blob
+        {
             this.ValidarSeDispensado();
             return this._blob;
         }
 
-        public get Name(): string {
-            this.ValidarSeDispensado();
-            if (this._blob instanceof File) {
-                //if (this.IsHeic)
-                //{
-                //    return ArquivoUtil.TrocarExtensao(this._blob.name, ".jpeg");
-                //}
-                return this._blob.name;
+        public get Name(): string
+        {
+            if (this._name == null)
+            {
+                this._name = this.RetornarNomeArquivo();
             }
-
-            return this._nomeArquivo ?? this.RetornarNomeGenerico();
+            return this._name;
         }
 
 
-        public get NameWithOutExtension(): string {
+        public get NameWithOutExtension(): string
+        {
             return ArquivoUtil.RetornarNomeArquivoSemExtensao(this.Name);
         }
 
-        public get Size(): number {
-            this.ValidarSeDispensado();
-            return this._blob.size;
+        public get Size(): number
+        {
+            if (this._size == null)
+            {
+                this.ValidarSeDispensado();
+                this._size = this._blob.size;
+            }
+            return this._size;
         }
 
-        public get UrlBlob(): string {
-            this.ValidarSeDispensado();
-            if (this._urlBlob == null) {
+        public get UrlBlob(): string
+        {
+            if (this._urlBlob == null)
+            {
+                this.ValidarSeDispensado();
                 this._urlBlob = window.URL.createObjectURL(this._blob);
             }
             return this._urlBlob;
         }
 
-        public get UrlIcone(): string {
+        public get UrlIcone(): string
+        {
             return $Configuracao.UrlIcone + this.Extensao;
         }
 
-        public get Type(): string {
-            this.ValidarSeDispensado();
-            return this._blob.type;
+        public get Type(): string
+        {
+            if (this._type == null)
+            {
+                this.ValidarSeDispensado();
+                this._type = this._blob.type;
+            }
+            return this._type;
         }
 
-        public get Extensao(): string {
-            return ArquivoUtil.RetornarExtensaoArquivo(this.name);
+        public get Extensao(): string
+        {
+            return ArquivoUtil.RetornarExtensaoArquivo(this.Name);
         }
 
-        public get MimeType(): EnumMimeType {
-            return ArquivoUtil.RetornarMineTypeEnum(this.name);
+        public get MimeType(): EnumMimeType
+        {
+            return ArquivoUtil.RetornarMineTypeEnum(this.Name);
         }
 
-        public get InfoImagemInterno(): IInformacaoImagem {
+        public get InfoImagemInterno(): IInformacaoImagem
+        {
             return this._informacaoImagem;
         }
 
-        public constructor(blob: Blob, nomeArquivo?: string) {
+        public constructor(blob: Blob, nomeArquivo: string = null)
+        {
             super();
-            if (!(blob instanceof Blob)) {
+            if (!(blob instanceof Blob))
+            {
                 throw new Erro("O argumento blob não foi definido");
-
             }
             this._blob = blob;
             this._nomeArquivo = nomeArquivo;
         }
 
-        public Slice(start?: number, end?: number, contentType?: string): SnBlob {
+        public Slice(start?: number, end?: number, contentType?: string): SnBlob
+        {
             this.ValidarSeDispensado();
             return new SnBlob(this._blob.slice(start, end, contentType));
         }
 
-        public ArrayBuffer(): Promise<ArrayBuffer> {
+        public async ArrayBuffer(): Promise<ArrayBuffer>
+        {
             this.ValidarSeDispensado();
-            return this._blob.arrayBuffer();
+            if (this._buffer == null)
+            {
+                this._buffer = await this._blob.arrayBuffer();
+            }
+            return this._buffer;
         }
 
-        public get Stream(): ReadableStream {
+        public get Stream(): ReadableStream
+        {
             this.ValidarSeDispensado();
             return this._blob.stream();
         }
 
-        public async TextoAsync(): Promise<string> {
+        public async TextoAsync(): Promise<string>
+        {
             this.ValidarSeDispensado();
             return this._blob.text();
         }
 
-        private ValidarSeDispensado(): void {
-            if (this._isDispensado) {
-                throw new Erro("O objeto já foi dispensado");
+        private ValidarSeDispensado(): void
+        {
+            if (this._isDispensado)
+            {
+                DebugUtil.Break(`O ${this.constructor.name} ${this._type ?? ""} ${this._name ?? ""} objeto já foi dispensado`);
+                throw new Erro(`O ${this.constructor.name} objeto já foi dispensado`);
             }
         }
 
-        public async ChecksumAsync(): Promise<string | Error> {
-            if (this._checksum == null) {
+        public async ChecksumAsync(): Promise<string | Error>
+        {
+            if (this._checksum == null)
+            {
                 const checksum = await w.Checksum.RetornarChecksumAsync(this._blob);
-                if (typeof checksum === "string" && u.Md5Util.IsMd5(checksum)) {
+                if (typeof checksum === "string" && u.Md5Util.IsMd5(checksum))
+                {
                     this._checksum = checksum;
                 }
                 return checksum;
@@ -119,103 +156,134 @@
             return this._checksum;
         }
 
-        public RevogarUrlBlob(): void {
-            if (this._urlBlob != null) {
-                window.URL.revokeObjectURL(this._urlBlob);
-                this._urlBlob = null;
-            }
-        }
-
-        public override Equals(obj: SnBlob): boolean {
-            if (obj instanceof SnBlob) {
+        public override Equals(obj: SnBlob): boolean
+        {
+            if (obj instanceof SnBlob)
+            {
                 return this._blob === obj._blob;
             }
             return false;
         }
 
-        public async RetornarInfoImagemAsync(): Promise<IInformacaoImagem> {
-            if (this._informacaoImagem == null) {
+        public async RetornarInfoImagemAsync(): Promise<IInformacaoImagem>
+        {
+            if (this._informacaoImagem == null)
+            {
                 this._informacaoImagem = await this.RetornarInfoImagemInternoAsync();
             }
             return this._informacaoImagem;
         }
 
-        private async RetornarInfoImagemInternoAsync(): Promise<IInformacaoImagem> {
+        private async RetornarInfoImagemInternoAsync(): Promise<IInformacaoImagem>
+        {
             const informacaoImagem = await w.InformacaoImagemWorker.RetornarInformacaoImagemAsync(this);
             const checksum = await this.ChecksumAsync();
-            if (typeof checksum === "string") {
+            if (typeof checksum === "string")
+            {
                 informacaoImagem.ChecksumArquivoLocal = checksum;
             }
             return informacaoImagem;
         }
 
-        public SalvarComo(nomeArquivo: string) {
+        public SalvarComo(nomeArquivo: string)
+        {
             Salvar.SalvarComo(this.Blob, nomeArquivo);
         }
 
-        public AtribuirInformacaoImagem(informacaoImagem: IInformacaoImagem) {
+        public AtribuirInformacaoImagem(informacaoImagem: IInformacaoImagem)
+        {
             this._informacaoImagem = informacaoImagem;
         }
 
         //#region nativa blob
 
-        public get name(): string {
-            if (this.Blob instanceof File) {
-                return this.Blob.name;
-            }
+        public get name(): string
+        {
             return this.Name;
         }
 
 
-        public get size(): number {
-            return this.Blob.size;
+        public get size(): number
+        {
+            return this.Size;
         }
 
-        public get type(): string {
-            return this.Blob.type;
+        public get type(): string
+        {
+            return this.Type;
         }
 
-        public arrayBuffer(): Promise<ArrayBuffer> {
-            return this.Blob.arrayBuffer();
+        public arrayBuffer(): Promise<ArrayBuffer>
+        {
+            return this.ArrayBuffer();
         }
 
-        public slice(start?: number, end?: number, contentType?: string): Blob {
+        public slice(start?: number, end?: number, contentType?: string): Blob
+        {
+            this.ValidarSeDispensado();
             return this.Blob.slice(start, end, contentType);
         }
 
-        public stream(): ReadableStream {
+        public stream(): ReadableStream
+        {
+            this.ValidarSeDispensado();
             return this.Blob.stream();
         }
-        public text(): Promise<string> {
+
+        public text(): Promise<string>
+        {
+            this.ValidarSeDispensado();
             return this.Blob.text();
         }
 
-        public override toString(): string {
-            return `SnBlob-${this.name}-${this.size}b`;
+        public override toString(): string
+        {
+            if (this._isDispensado)
+            {
+                return `SnBlob-${this._name}-${this._size}b`;
+            }
+            return `SnBlob-${this.Name}-${this.Size}b`;
         }
         //#endregion
 
-        public RevokeUrlBlob() {
-            if (this._urlBlob != null) {
+        public RevokeUrlBlob()
+        {
+            if (this._urlBlob != null)
+            {
+                DebugUtil.Break("SnBlob.RevokeUrlBlob");
                 window.URL.revokeObjectURL(this._urlBlob);
             }
             this._urlBlob = null;
             delete this._urlBlob;
         }
 
-        private RetornarNomeGenerico(): string {
-            if (this.IsHeic) {
+        private RetornarNomeArquivo(): any
+        {
+            this.ValidarSeDispensado();
+            if (this._blob instanceof File)
+            {
+                return this._blob.name;
+            }
+            return this._nomeArquivo ?? this.RetornarNomeGenerico();
+        }
+
+        private RetornarNomeGenerico(): string
+        {
+            if (this.IsHeic)
+            {
                 return `[blob]-${(this.size)}.jpeg`;
             }
             const extensao = ArquivoUtil.RetornarExtensaoArquivo(this._blob.type);
             return `[blob]-${(this.size)} ${extensao}`;
         }
 
-        public override Dispose(): void {
-
-            if (!this._isDispensado) {
+        public override Dispose(): void
+        {
+            if (!this._isDispensado)
+            {
                 this.RevokeUrlBlob();
                 delete this._blob;
+                delete this._buffer;
                 this._isDispensado = true;
             }
         }
