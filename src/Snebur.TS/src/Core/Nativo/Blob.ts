a69ce7d4587c1055f7049556ddb75ef3a2276494
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-06-23 11:40:32 -0300

    Polyfill Blob.prototype.arrayBuffer if missing
    Added a Snebur namespace in Blob.ts to polyfill Blob.prototype.arrayBuffer using FileReader when not available. Also added a blank line (with BOM) to IBlob.ts with no functional impact.

---
+++ src/Snebur.TS/src/Core/Nativo/Blob.ts
@@ -0,0 +1,16 @@
+﻿namespace Snebur
+{
+    if(Blob.prototype.arrayBuffer === undefined)
+    {
+        Blob.prototype.arrayBuffer = function(): Promise<ArrayBuffer>
+        {
+            return new Promise((resolve, reject) =>
+            {
+                const reader = new FileReader();
+                reader.onload = () => resolve(reader.result as ArrayBuffer);
+                reader.onerror = () => reject(reader.error);
+                reader.readAsArrayBuffer(this);
+            });
+        };
+    }
+}
